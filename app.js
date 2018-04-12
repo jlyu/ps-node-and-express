181a9dfffc40394d8dc507fe2cef1459e620bec9
Chain Yu <yujnln@gmail.com>
2018-04-12 22:06:27 +0800

    Route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 //app.use(express.static('src/views'));
@@ -11,6 +12,20 @@ app.set('views', './src/views');
 //app.engine('.hbs', handlebars({extname: '.hbs'}));
 
 app.set('view engine', 'ejs'); //'jade','.hbs'
+
+bookRouter.route('/')
+    .get(function(req, res){
+        res.send('Hello Books');
+});
+
+bookRouter.route('/single')
+    .get(function(req, res){
+        res.send('Hello Book');
+});
+
+app.use('/Books', bookRouter);
+
+
 
 app.get('/', function(req, res) {
     res.render('index', {
