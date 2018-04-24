749027b661540e6e53ee77ee839eaddfe2e83c02
Chain Yu <yujnln@gmail.com>
2018-04-24 23:07:19 +0800

    Insert Many

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,22 @@
 var express = require('express');
-
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
 
 var router = function (nav) {
 
     adminRouter.route('/addBooks')
         .get(function (req, res) {
-            res.send('inserting books');
+
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.insertMany(books, function (err, results) {
+                    res.send(results);
+                    db.close();
+                });
+            });
+
+            //res.send('inserting books');
         });
 
     return adminRouter;
