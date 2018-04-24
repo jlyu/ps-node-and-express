9aabd4294e3fb6082e25b9d57d9ebdcb8f862a9a
Chain Yu <yujnln@gmail.com>
2018-04-24 23:18:47 +0800

    select many

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,83 +1,92 @@
 var express = require('express');
 
 var bookRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
 
 var router = function (nav) {
 
-    var books = [
-        {
-            title: 'War and Peace',
-            genre: 'Historical Fiction',
-            author: 'Lev Nikolayevich Tolstoy',
-            read: false
+  var books = [
+    {
+      title: 'War and Peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
         },
-        {
-            title: 'Les Misérables',
-            genre: 'Historical Fiction',
-            author: 'Victor Hugo',
-            read: false
+    {
+      title: 'Les Misérables',
+      genre: 'Historical Fiction',
+      author: 'Victor Hugo',
+      read: false
         },
-        {
-            title: 'The Time Machine',
-            genre: 'Science Fiction',
-            author: 'H. G. Wells',
-            read: false
+    {
+      title: 'The Time Machine',
+      genre: 'Science Fiction',
+      author: 'H. G. Wells',
+      read: false
         },
-        {
-            title: 'A Journey into the Center of the Earth',
-            genre: 'Science Fiction',
-            author: 'Jules Verne',
-            read: false
+    {
+      title: 'A Journey into the Center of the Earth',
+      genre: 'Science Fiction',
+      author: 'Jules Verne',
+      read: false
         },
-        {
-            title: 'The Dark World',
-            genre: 'Fantasy',
-            author: 'Henry Kuttner',
-            read: false
+    {
+      title: 'The Dark World',
+      genre: 'Fantasy',
+      author: 'Henry Kuttner',
+      read: false
         },
-        {
-            title: 'The Wind in the Willows',
-            genre: 'Fantasy',
-            author: 'Kenneth Grahame',
-            read: false
+    {
+      title: 'The Wind in the Willows',
+      genre: 'Fantasy',
+      author: 'Kenneth Grahame',
+      read: false
         },
-        {
-            title: 'Life On The Mississippi',
-            genre: 'History',
-            author: 'Mark Twain',
-            read: false
+    {
+      title: 'Life On The Mississippi',
+      genre: 'History',
+      author: 'Mark Twain',
+      read: false
         },
-        {
-            title: 'Childhood',
-            genre: 'Biography',
-            author: 'Lev Nikolayevich Tolstoy',
-            read: false
+    {
+      title: 'Childhood',
+      genre: 'Biography',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
         }
     ];
 
-    bookRouter.route('/')
-        .get(function (req, res) {
-            res.render('bookListView', {
-                title: 'hello books',
-                list: ['a', 'b'],
-                nav:  nav,
-                books: books
-            });
+  bookRouter.route('/')
+    .get(function (req, res) {
+
+      var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function (err, db) {
+        var collection = db.collection('books');
+        collection.find({}).toArray(function (err, results) {
+          res.render('bookListView', {
+            title: 'Books',
+            nav: nav,
+            books: results
+          });
         });
+      });
 
-    bookRouter.route('/:id')
-        .get(function (req, res) {
-            var id = req.params.id;
 
-            res.render('bookView', {
-                title: 'hello books',
-                list: ['a', 'b'],
-                nav: nav,
-                book: books[id]
-            });
-        });
+    });
 
-    return bookRouter;
+  bookRouter.route('/:id')
+    .get(function (req, res) {
+      var id = req.params.id;
+
+      res.render('bookView', {
+        title: 'hello books',
+        list: ['a', 'b'],
+        nav: nav,
+        book: books[id]
+      });
+    });
+
+  return bookRouter;
 };
 
 
